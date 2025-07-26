4cbf7cc433dd6c522aed9200bf67e33aa9b0dc44
AG15126 <92546149+AG15126@users.noreply.github.com>
2025-07-26 12:14:33 +0530

    Update stock_predictor_app.py

--- stock_predictor_app.py
+++ stock_predictor_app.py
@@ -5,8 +5,8 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
 
 # Helper: Load data
